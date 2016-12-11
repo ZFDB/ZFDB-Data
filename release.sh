85003286700f4d1aaf6963e48776f0c381331dfa
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-12-12 00:24:09 +0530

    Initial commit
    release.sh, to --/release/-- the builds after successful completion of "Grunt-Task(s):build", for "ZFDB®".
    Usage: $ sh release.sh

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# ===========================++== ZFDB® ==++==========================
+#     A manually curated dedicated zinc finger proteins database.
+#
+#                      GNU GENERAL PUBLIC LICENSE
+#                     http://www.gnu.org/licenses/
+#
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#     Copyright © 2006 - 2016, Prabhat Kumar. All rights reserved.
+# Copyright © 2014 - 2016, Sequømics Corporation, All rights reserved.
+# ====================================================================
