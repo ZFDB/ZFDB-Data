a94de2f16ceeb094cb193f9cf256762de3390266
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-12-12 14:06:56 +0530

    Initial commit
    grunt-contrib-compress — Compress files and folders.
    Usage: $ npm install grunt-contrib-compress --save-dev | Help: https://github.com/gruntjs/grunt-contrib-compress

---
+++ tasks/options/compress.js
@@ -0,0 +1,30 @@
+/*!
+ * ----------
+ * ZFDB®-Data
+ * ----------
+ * Data of ZFDB — A manually curated dedicated zinc finger proteins database.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 11-Dec-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-contrib-compress
+ * @build     : SEED™ — Örebro
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "ZFDB®-Data".
+ * ___________________________________________________________________________
+ */
