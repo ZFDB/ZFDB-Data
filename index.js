bf5c06fe51d88abc9d61490df137ac9b975e481f
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-12-12 16:26:09 +0530

    Initial commit
    --/The Heart of Data System/-- of "ZFDB®-Data". | License — GPL, version 3.0 — http://www.gnu.org/licenses/.

---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+/*!
+ * ----------
+ * ZFDB®-Data
+ * ----------
+ * Data of ZFDB — A manually curated dedicated zinc finger proteins database.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 11-Dec-2016
+ * @license   : GPL, version 3.0 [http://www.gnu.org/licenses/]
+ * @require   : Node.js®
+ * @require   : NPM
+ * @build     : SEED™ — Örebro
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Data System/-- of "ZFDB®-Data".
+ * ___________________________________________________________________________
+ */
